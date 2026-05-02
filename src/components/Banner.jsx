dea819fd41f6979ca073fe1f043cc3fd1fc733d7
Shohan111369 <sarkershohan346@gmail.com>
2026-05-02 17:08:59 +0800

    Also design chnage banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,27 +3,23 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('data:image/svg+xml,%3Csvg xmlns=%27http://www.w3.org/2000/svg%27 width=%2740%27 height=%2740%27 viewBox=%270 0 40 40%27%3E%3Cpath d=%27M40 0H0V40%27 fill=%27none%27 stroke=%23e5e7eb%27 stroke-width=%271%27/%3E%3C/svg%3E')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl">
+    <div className="bg-[url('https://images.unsplash.com/photo-1600607687939-ce8a6c25118c')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl">
       {/* Overlay */}
       <div className="w-full h-full rounded-lg bg-black/50 flex items-center ">
         <div className="max-w-7xl mx-auto px-6 text-white">
           <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-2xl">
-            Turn Ideas into Stunning AI Art
+            Explore Modern Tiles for Every Space
           </h1>
-          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
-            Generate high-quality images from simple text prompts.
+
+          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-600">
+            Find premium tile designs, view detailed previews, and discover
+            styles that perfectly match your interior and architectural ideas.
           </p>
 
           <div className="flex gap-4">
             <Link href="#">
               <Button className="bg-linear-to-r from-pink-500 via-purple-500 bg-red-500">
-                Generate Now
-              </Button>
-            </Link>
-
-            <Link href="/pricing">
-              <Button variant="outline" className="text-white">
-                View Pricing
+                Browser Now
               </Button>
             </Link>
           </div>
