c4983468aefbe7866bfebae30e764c1d34f7ad46
Shohan111369 <sarkershohan346@gmail.com>
2026-05-02 16:04:02 +0800

    Now changed auth file

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -17,9 +17,13 @@ export const auth = betterAuth({
     enabled: true,
   },
   socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID , 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        }, 
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
+  },
+  trustedOrigins: [
+        process.env.BETTER_AUTH_URL,
+        'http://localhost:3000'
+    ],
 });
